aebdeafbe42932943e4bdb7ff0ee658117e377a4
Saurav Yadav <111695718+rao119349@users.noreply.github.com>
2026-03-13 21:55:20 +0530

    Duplicate Navbar component in Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
-      <Navbar />
+      <Navbar /><Navbar />
       <Hero />
       <About />
       <Work />
