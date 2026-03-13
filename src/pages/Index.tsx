578d1153e9113e7734d1f2562b3bf74135cc8fe9
Saurav Yadav <sauravyadav1193@gmail.com>
2026-03-13 22:25:21 +0530

    index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,8 @@ import Footer from "@/components/Footer";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
-      <Navbar /><Navbar />
+      <Navbar />
+      <Hero />
       <Hero />
       <About />
       <Work />
